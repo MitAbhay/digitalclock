1ca87a55e134ccc29db69f9238e727f3e1bc1a24
MitAbhay <leader.abhay002@gmail.com>
2021-09-27 11:17:59 +0530

    this is my digital clock that i made for react spec interview

---
+++ src/clock.js
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from "react";
+import './App.css';
+
+const date = new Date();
+export default function App() {
+  const [dateTime, setDateTime] = useState({
+    hours: date.getHours(),
+    minutes: date.getMinutes(),
+    seconds: date.getSeconds()
+  });
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      const date = new Date();
+
+      setDateTime({
+        hours: date.getHours(),
+        minutes: date.getMinutes(),
+        seconds: date.getSeconds()
+      });
+    }, 1000);
+    return () => clearInterval(timer);
+  }, []);
+
+  return (
+    <div className="App">
+      <div className="hour">
+        {dateTime.hours}:
+      </div>
+      <div className="minute">
+{dateTime.minutes}:
+      </div>
+      
+      <div className="seconds">
+{dateTime.seconds}
+    </div>
+    </div>
+
+  );
+}
